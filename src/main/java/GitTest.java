cd5327b017acf190ddcf9734b95d2ee01abb661c
zs <zs@qq.com>
2022-04-11 17:08:34 +0800

    firsr commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author zs
+ * path: spark-PACKAGE_NAME-GitTest
+ * date: 2022/4/11 17:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
